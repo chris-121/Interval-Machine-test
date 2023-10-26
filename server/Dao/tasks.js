7bb10a7a6def8a903dc818a50fcbe4a637afdbf2
Chris Jacob <chrisjacob876@gmail.com>
2023-10-26 18:25:40 +0530

    dao added

---
+++ server/Dao/tasks.js
@@ -0,0 +1,9 @@
+module.exports = {
+  getAllTasks: async () => {},
+
+  createTask: async (taskData) => {},
+
+  updateTask: async (taskData, whereObj) => {},
+
+  deleteTask: async (whereObj) => {},
+};
